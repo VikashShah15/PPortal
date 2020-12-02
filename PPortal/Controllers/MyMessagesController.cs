037846fab86e4cc302b53b0602c427fc956d04e9
Vikas Shah <vshah1506@gmail.com>
2020-12-02 12:18:22 +0530

    New Changes Added

---
+++ PPortal/Controllers/MyMessagesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PPortal.Controllers
+{
+    public class MyMessagesController : Controller
+    {
+        // GET: MyMessages
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PPortal/Controllers/RequestRefillController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PPortal.Controllers
+{
+    public class RequestRefillController : Controller
+    {
+        // GET: RequestRefill
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
